cb8868499bba03371590f5954a9b123610030ff3
Akshatt10 <akshattyagi1010@gmail.com>
2025-03-28 07:26:00 +0530

    routes added

--- Batman/Batman/views.py
+++ Batman/Batman/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from django.urls import path
 
 def home(request):
-    return HttpResponse("Hello, Batman! This is the home page.")
-
+    # return HttpResponse("Hello, Batman! This is the home page.")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("Hello, Batman! This is the aboutt page.")
